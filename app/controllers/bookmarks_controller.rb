00ed2296612470f2ba7c264b903bbaec6e903bd0
mauricio pineda <map1900@outlook.com>
2021-08-11 15:12:39 -0400

    started bookmarks silos

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,30 @@
 class BookmarksController < ApplicationController
+  before_action :find_bookmark, except: [ :index, :new, :create ]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmark_path(@bookmark)
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+
+  def find_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,7 +3,5 @@ class Bookmark < ApplicationRecord
   validates :movie, presence: true, uniqueness: { scope: :list }
   belongs_to :list
   validates :list, presence: true, uniqueness: true
-  validates :comment, presence: true, length: {minimum: 6}
-  
-  
+  validates :comment, presence: true, length: { minimum: 6 }
 end

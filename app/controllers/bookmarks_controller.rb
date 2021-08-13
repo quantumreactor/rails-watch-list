267c0a9b78a0b92580f1c8e322ac7a37d1c87bc4
mauricio pineda <map1900@outlook.com>
2021-08-13 17:15:54 -0400

    more ui touches

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -13,8 +13,13 @@ class BookmarksController < ApplicationController
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
-    flash[:notice] = @bookmark.errors.full_messages.to_sentence unless @bookmark.save
-    redirect_to list_path(@list)
+    #flash[:notice] = @bookmark.errors.full_messages.to_sentence unless @bookmark.save
+    # aqui despliegue el flash
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def show; end
